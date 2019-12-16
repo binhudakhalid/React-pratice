1fb174bc067f912088c846b8595f561ad8fbfd97
binhudakhalid <khalid.bin.huda@hotmail.com>
2019-12-16 20:23:26 +0500

    1.13- before handler argment

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,7 +4,8 @@ class Counter extends Component {
   state = {
     count: 0,
     imageUrl: "https://picsum.photos/200",
-    tags: ["tag1", "tag2", "tag3"]
+    tags: ["tag1", "tag2", "tag3"],
+    age: 25
   };
   formatCount() {
     const { count } = this.state;
@@ -16,13 +17,41 @@ class Counter extends Component {
     classes += this.state.count === 0 ? "warning" : "primary";
     return classes;
   }
+  handleIncrement = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+
+    console.log(this.state.count);
+  };
+
+  addAge = () => {
+    console.log("age");
+    this.setState({
+      age: this.state.age + 5
+    });
+  };
 
   render() {
     return (
       <div>
         <img src={this.state.imageUrl} />
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+
+        <div>{this.state.age}</div>
+        <button
+          onClick={this.addAge}
+          style={{ backgroundColor: "red", color: "white" }}
+        >
+          Age
+        </button>
+
         <ul>
           {this.state.tags.map(tag => (
             <li key={tag}>{tag + "asda"}</li>

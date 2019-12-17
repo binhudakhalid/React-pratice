79141af42adbeefb7592937381441f96063d85f8
binhudakhalid <khalid.bin.huda@hotmail.com>
2019-12-17 18:22:03 +0500

    before single source of truth

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: this.props.value,
+    count: this.props.counter.value,
     imageUrl: "https://picsum.photos/200",
     tags: ["tag1", "tag2", "tag3"],
     age: 25
@@ -29,7 +29,6 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        <h4>{this.props.id}</h4>
         <img src={this.state.imageUrl} />
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
@@ -37,6 +36,13 @@ class Counter extends Component {
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
 
         <div>{this.state.age}</div>
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -12,11 +12,32 @@ class Counters extends Component {
     ]
   };
 
+  handleDelete = counterId => {
+    console.log("eventlog called    ", counterId);
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+
+    this.setState({ counters });
+  };
+
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
+
         {this.state.counters.map(c => (
-          <Counter key={c.id} value={c.value} selected id={c.id} />
+          <Counter key={c.id} onDelete={this.handleDelete} counter={c} />
         ))}
       </div>
     );
@@ -24,3 +45,4 @@ class Counters extends Component {
 }
 
 export default Counters;
+//https://youtu.be/Ke90Tje7VS0
